b57d85ab0eeb2bedd482cdd0e3c63835bc66c10b
yulepetrilli <yulepetrilli@gmail.com>
2022-10-26 17:24:09 -0400

    completed header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="d-flex justify-content-center my-4">
-        <div class="pt-2 pe-2">Type Pokemon name or ID:</div>
-        <input class="form-control" style="max-width:300px" type="text" >
+    <div class="row bg-danger justify-content-center" >
+      <div class="d-flex flex-column my-4" style="max-width:450px">
+        <div class="text-center text-white"><h1>Pokedex</h1></div>
+        <div class="my-3">
+          <input class="form-control" type="text" placeholder="Type pokemon name or ID">
+        </div>
+        <div class="text-center">
+          <button type="submit" class="btn btn-success" style="max-width:150px">Search Pokemon</button>
+        </div>
       </div>
     </div>
   </div>
